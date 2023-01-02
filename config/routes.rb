d8d5cd023c97d6710c0d0b48440da326586ce36d
PraisesPJMT <praisesmusa@outlook.com>
2023-01-02 23:38:17 +0100

    Make `Users#show` root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
