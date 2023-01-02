c42a80b7ff62a6511478ff263ee1c40875ea757a
PraisesPJMT <praisesmusa@outlook.com>
2023-01-02 23:36:38 +0100

    Add `User` controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+  end
+
+  private
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :trackable
 
   validates :name, presence: true
 end
