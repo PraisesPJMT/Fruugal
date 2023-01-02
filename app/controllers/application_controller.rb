97871322c48a1a04c4c3e369be7be59fcbe4d268
PraisesPJMT <praisesmusa@outlook.com>
2023-01-03 00:37:14 +0100

    Modify `Splash` page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
+
+  def after_sign_in_path_for(_resource_or_scope)
+    user_url(current_user)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def splash
+    if current_user
+      redirect_to user_url(current_user)
+    end
   end
 end
