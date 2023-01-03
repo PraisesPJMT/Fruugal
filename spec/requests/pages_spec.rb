45ba700b4da2821bdeafafaacd109ea052c395ee
PraisesPJMT <praisesmusa@outlook.com>
2023-01-03 12:58:51 +0100

    Style app `Splash` screen

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", type: :request do
-  describe "GET /splash" do
-    it "returns http success" do
-      get "/pages/splash"
+RSpec.describe 'Pages', type: :request do
+  describe 'GET /splash' do
+    it 'returns http success' do
+      get '/pages/splash'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/views/pages/splash.html.erb_spec.rb
+++ spec/views/pages/splash.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "pages/splash.html.erb", type: :view do
+RSpec.describe 'pages/splash.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
