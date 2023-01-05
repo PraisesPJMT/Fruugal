bdf0b81b223e28f5092a00f923e402ea59b0c48d
PraisesPJMT <praisesmusa@outlook.com>
2023-01-05 15:58:24 +0100

    Add unit tests for `Expense` controller

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,30 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses:', type: :request do
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/transactions/create'
-      expect(response).to have_http_status(:success)
-    end
+  include Devise::Test::IntegrationHelpers
+  before :each do
+    @user = User.new(name: 'John Doe', email: 'test@email.com', password: 'password', password_confirmation: 'password')
+    @user.save
+    sign_in @user
+    @category = Category.new(name: 'Test Category', icon: 'test_icon.url')
+    @category.user = @user
+    @category.save
+    @expense = Expense.new(name: 'Test Expense', amount: 5)
+    @expense.user = @user
+    @expense.save
   end
 
-  describe 'GET /edit' do
-    it 'returns http success' do
-      get '/transactions/edit'
+  context 'GET new, & /edit' do
+    it 'Expense new page success' do
+      get new_expense_path
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/transactions/destroy'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/transactions/new'
+    it 'Expense edit page success' do
+      get edit_expense_path(@expense)
       expect(response).to have_http_status(:success)
     end
   end
