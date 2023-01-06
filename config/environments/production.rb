5334f1016170ab1cfce193bfc6a3447f78e2f2d5
PraisesPJMT <praisesmusa@outlook.com>
2023-01-06 01:50:26 +0100

    Reconfigure Rails Web Services

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "fruugal-production.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -22,7 +23,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
